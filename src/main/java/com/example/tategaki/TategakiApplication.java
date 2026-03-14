5435e051ffe8a4a2a6d43e2631b51b6c966a171c
sakurai <mene10morim@gmail.com>
2026-03-14 11:40:04 +0900

    first commit

---
+++ src/main/java/com/example/tategaki/TategakiApplication.java
@@ -0,0 +1,13 @@
+package com.example.tategaki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TategakiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TategakiApplication.class, args);
+	}
+
+}
