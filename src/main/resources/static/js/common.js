361a1064a16a2c89e815892e13b414195da95088
sakurai <mene10morim@gmail.com>
2026-03-18 21:34:05 +0900

    topic/30修正

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,11 +1,12 @@
 
 const saveBtn = document.getElementById('save-settings');
-saveBtn.addEventListener('click', () => {
-	const selectedFont = document.querySelector('input[name="font"]:checked').value;
-	localStorage.setItem("userFont", selectedFont);
-	alert('保存完了');
-});
-
+if (saveBtn) {
+	saveBtn.addEventListener('click', () => {
+		const selectedFont = document.querySelector('input[name="font"]:checked').value;
+		localStorage.setItem("userFont", selectedFont);
+		alert('保存完了');
+	});
+}
 
 const savedFont = localStorage.getItem('userFont');
 if (savedFont) {
