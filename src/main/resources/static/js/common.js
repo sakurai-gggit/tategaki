c6a938f460a384c1224f566099ca4c662302821f
sakurai <mene10morim@gmail.com>
2026-03-24 12:13:20 +0900

    設定ページにそれぞれのフォントを適用するjsを実装

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -24,3 +24,26 @@ if (drawerBtn && drawerMenu) {
 		drawerBtn.classList.toggle('open');
 	});
 }
+
+
+const sampleText = "「そうだ。おや、あの河原は月夜だろうか。」そっちを見ますと、青白く光る銀河の岸に、銀いろのススキが、もうまるで一面、風にさらさらさらさら、ゆれてうごいて、波を立てているのでした。";
+const fonts = [
+	{ id: "HannariMincho", name: "はんなり明朝" },
+	{ id: "WDXL", name: "WDXL Lubrifont" }
+];
+
+const container = document.getElementById('font-list');
+
+const fontListContent = fonts.map(font => {
+	return `
+	<li>
+		<label style="font-family:${font.id}">
+			<input type="radio" name="font" value="${font.id}">
+			<span>${font.name}</span>
+			<div>${sampleText}</div>
+		</label>
+	</li>
+	`
+}).join("");
+
+container.innerHTML = fontListContent;
